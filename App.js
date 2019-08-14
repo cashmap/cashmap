f6fecbf867b27274718bd7513352da4985a8876f
Chris Choi <chc5139@gmail.com>
2019-08-14 10:37:08 -0400

    firebase setup

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { FirebaseWrapper } from "./firebase/firebase";
+import { firebaseConfig } from "./firebase/config";
 
 export default function App() {
+  FirebaseWrapper.GetInstance().Initialize(firebaseConfig);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -12,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ firebase/firebase.js
@@ -0,0 +1,66 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+// WITH ALL COMPONENTS - ANYTHING I WANT TO DO PERTAINING TO FIREBASE, I CAN USE AN INSTANCE OF THIS
+export class FirebaseWrapper {
+  constructor() {
+    this.initialized = false; // calling to this firebase wrapper, if its not, we're going to initialize it
+    this._firebaseInstance = null; // instance of our npm package
+    this._firebaseWrapperInstance = null; // instance of our wrapper
+    this._firestore = null;
+  }
+
+  Initialize(config) {
+    if (!this.initialized) {
+      // initialize firebase
+      this._firebaseInstance = firebase.initializeApp(config);
+      this._firestore = firebase.firestore();
+      this.initialized = true;
+      console.log("CURRENTLY WORKING");
+    } else {
+      console.log("already initialized!");
+    }
+  }
+
+  static GetInstance() {
+    // static - directly call without having to instantiate our method first
+    if (null == this._firebaseWrapperInstance) {
+      this._firebaseWrapperInstance = new FirebaseWrapper();
+    } else {
+      // Already initialized, nothing more to do here
+    }
+
+    return this._firebaseWrapperInstance;
+  }
+
+  //this is a test for our database to see a post
+  async CreateNewDocument(collectionPath, doc) {
+    try {
+      const ref = this._firestore.collection(collectionPath).doc();
+      const timestamp = firebase.firestore.Timestamp.now().toDate();
+      return await ref.set({ ...doc, createdAt: timestamp, id: ref.id });
+    } catch (error) {
+      console.log("something went wrong", error);
+    }
+  }
+  //CREATE A LISTENER THAT FIREBASE CAN LISTEN ON DIFFERENT REFERENCES ON OUR DATABASE, ANYTHING THAT CHANGES ON OUR REFERENCE WE CAN DO A CALLBACK (STORE.SUBSCRIBE.. LETS SET OUR NEW STATE, SIMILAR TO FIREBASE)
+
+  async SetupCollectionListener(collectionPath, callback) {
+    try {
+      console.log("calling SetupCollectionListener");
+      await this._firestore
+        .collection(collectionPath)
+        .orderBy("createdAt", "desc")
+        .onSnapshot(querySnapshot => {
+          let container = [];
+          querySnapshot.forEach(doc => {
+            container.push(doc.data());
+          });
+          return callback(container);
+        });
+    } catch (error) {
+      console.log("oh no! something went wrong!", error);
+    }
+  }
+  //anytime we get data from db, it comes in form of snapshot
+}
