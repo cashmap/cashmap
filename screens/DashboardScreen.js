a22e6b982c5891e602d3a9f53e3852df9a484be4
David Rusyniak <davidr@Davids-MacBook-Pro.local>
2019-08-20 18:56:31 -0400

    map renders upon Plaid login

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
-import firebase from "firebase";
-import PlaidAuthenticator from "react-native-plaid-link";
-import PlaidScreen from "./PlaidScreen";
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import MapView from 'react-native-maps';
+import PlaidAuthenticator from 'react-native-plaid-link';
+import PlaidScreen from './PlaidScreen';
 import {
   createAppContainer,
   createSwitchNavigator,
-  createDrawerNavigator
-} from "react-navigation";
-
+  createDrawerNavigator,
+  NavigationEvents,
+} from 'react-navigation';
 import {
   View,
   Text,
   StyleSheet,
   ActivityIndicator,
-  Button
-} from "react-native";
-
+  Button,
+} from 'react-native';
 export default class DashboardScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: "",
-      status: ""
+      data: '',
+      status: '',
     };
   }
   onMessage = data => {
@@ -38,16 +38,42 @@ export default class DashboardScreen extends Component {
           clientName="CashMap"
           selectAccount={false}
         />
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 40.705307,
+            longitude: -74.009088,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+          }}
+        >
+          <MapView.Marker
+            coordinate={{ latitude: 40.705307, longitude: -74.009088 }}
+            title={'Fullstack'}
+            description={'Academy of Code'}
+          />
+        </MapView>
         <Button title="Sign Out" onPress={() => firebase.auth().signOut()} />
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    alignItems: 'center',
+    justifyContent: 'flex-end', //center
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
 });
