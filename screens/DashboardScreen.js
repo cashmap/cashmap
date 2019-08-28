0f1e5da61501552001946850e039fa804878c72d
Chris Choi <chc5139@gmail.com>
2019-08-28 11:57:28 -0400

    Ben here is our unexpected react behavior in dashboardscreen.js

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,58 +1,61 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
-import { Icon } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import PlaidAuthenticator from 'react-native-plaid-link';
-import getTransResult from './PlaidScreen';
-import Map from './Map';
-import MenuButton from '../components/MenuButton';
-const mapStyle = require('./jsons/darkmap');
+import React, { Component } from "react";
+import firebase from "firebase";
+import { Icon } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import PlaidAuthenticator from "react-native-plaid-link";
+import getTransResult from "./PlaidScreen";
+import Map from "./Map";
+import DatePicker from "react-native-datepicker";
+import MenuButton from "../components/MenuButton";
+const mapStyle = require("./jsons/darkmap");
 
 import {
   createAppContainer,
   createSwitchNavigator,
   createDrawerNavigator,
-  NavigationEvents,
-} from 'react-navigation';
+  NavigationEvents
+} from "react-navigation";
 import {
   View,
   Text,
   StyleSheet,
   ActivityIndicator,
-  Button,
-} from 'react-native';
+  Button
+} from "react-native";
 
 export default class DashboardScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: '',
-      status: '',
-      accesstoken: '',
+      data: "",
+      status: "",
+      accesstoken: "",
       transactions: {},
+      startDate: "2017-01-01",
+      endDate: "2019-01-01"
     };
   }
 
   componentDidMount() {
     firebase
       .firestore()
-      .collection('users')
+      .collection("users")
       .doc(firebase.auth().currentUser.providerData[0].uid)
       .get()
       .then(doc => {
         if (!doc.exists) {
-          console.log('No such document!');
+          console.log("No such document!");
         } else {
           let userAccessToken = doc.data().accesstoken;
           this.setState({
-            accesstoken: userAccessToken,
+            accesstoken: userAccessToken
           });
           console.log(this.state.accesstoken);
           this.transGetter();
         }
       })
       .catch(err => {
-        console.log('Error getting document', err);
+        console.log("Error getting document", err);
       });
   }
 
@@ -60,18 +63,39 @@ export default class DashboardScreen extends Component {
     const { data: getTransResult } = await firebase
       .functions()
 
-      .httpsCallable('getTrans')({
+      .httpsCallable("getTrans")({
       access_token: this.state.accesstoken,
-      start_date: '2017-01-01',
-      end_date: '2019-01-01',
+      start_date: "2017-01-01",
+      end_date: "2019-01-01"
     });
-    console.log('getTrans is Running!');
+    console.log("getTrans is Running!");
     if (getTransResult) {
       this.setState({ transactions: getTransResult });
     }
   }
 
+  async transUpdater(start, end) {
+    console.log("start Date: ", start);
+    console.log("end Date: ", end);
+    const { data: getTransResult } = await firebase
+      .functions()
+      .httpsCallable("getTrans")({
+      access_token: this.state.accesstoken,
+      start_date: start,
+      end_date: end
+    });
+    console.log("transUpdater is Running!");
+    if (getTransResult) {
+      this.setState({ transactions: getTransResult });
+      console.log(
+        "UPDATING STATE-------------------",
+        this.state.transactions.transactions
+      );
+    }
+  }
+
   render() {
+    console.log("DASHBOARD SCREEN STATE------------", this.state.transactions);
     if (this.state.transactions.transactions) {
       return (
         <View style={styles.container}>
@@ -85,29 +109,74 @@ export default class DashboardScreen extends Component {
             transactions={this.state.transactions.transactions}
             navigation={this.props.navigation}
           />
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between"
+            }}
+          >
+            <DatePicker
+              style={{ width: 150 }}
+              date={this.state.startDate} //initial date from state
+              mode="date" //The enum of date, datetime and time
+              placeholder="select start date"
+              format="YYYY-MM-DD"
+              minDate="2016-01-01"
+              maxDate="2019-01-01"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              showIcon={false}
+              onDateChange={date => {
+                this.setState({ startDate: date });
+              }}
+            />
+            <DatePicker
+              style={{ width: 150 }}
+              date={this.state.endDate} //initial date from state
+              mode="date" //The enum of date, datetime and time
+              placeholder="select end date"
+              format="YYYY-MM-DD"
+              minDate="2016-01-01"
+              maxDate="2019-01-01"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              showIcon={false}
+              onDateChange={date => {
+                this.setState({ endDate: date });
+              }}
+            />
+          </View>
+
+          <Button
+            title="Submit"
+            onPress={() =>
+              this.transUpdater(this.state.startDate, this.state.endDate)
+            }
+          />
         </View>
       );
     } else {
-      return <View style={{ backgroundColor: 'red' }}></View>;
+      return <View style={{ backgroundColor: "red" }} />;
     }
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-end', //center
-    position: 'absolute',
+    alignItems: "center",
+    justifyContent: "flex-end", //center
+    position: "absolute",
     top: 0,
     left: 0,
     bottom: 0,
-    right: 0,
+    right: 0
   },
   map: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     bottom: 0,
-    right: 0,
-  },
+    right: 0
+  }
 });
